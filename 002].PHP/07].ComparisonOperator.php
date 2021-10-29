34c85fe6e0da615e8118fad4ebc50473135596bd
Pawan Gupta <guptapawanro2017@gmail.com>
2021-10-29 12:06:36 +0530

    COmaparison operator

---
+++ 002].PHP/07].ComparisonOperator.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+
+==	Equal	                    $x == $y	    Returns true if $x is equal to $y	
+===	Identical	                $x === $y	    Returns true if $x is equal to $y, and they are of the same type	
+!=	Not equal	                $x != $y	    Returns true if $x is not equal to $y	
+<>	Not equal	                $x <> $y	    Returns true if $x is not equal to $y	
+!==	Not identical	            $x !== $y	    Returns true if $x is not equal to $y, or they are not of the same type	
+>	Greater than	            $x > $y	        Returns true if $x is greater than $y	
+<	Less than	                $x < $y	        Returns true if $x is less than $y	
+>=	Greater than or equal to	$x >= $y	    Returns true if $x is greater than or equal to $y	
+<=	Less than or equal to	    $x <= $y	    Returns true if $x is less than or equal to $y	
+<=>	Spaceship	                $x <=> $y	    Returns an integer less than, equal to, or greater than zero, depending on if $x is less than, equal to, or greater than $y.
+
+*/
+
+?>
