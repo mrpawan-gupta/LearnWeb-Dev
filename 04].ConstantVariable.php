cdd87a78ac2daea62e8f83d05e368d17c2c14bed
Pawan Gupta <guptapawanro2017@gmail.com>
2021-10-29 08:12:50 +0530

    Const. Variable

---
+++ 04].ConstantVariable.php
@@ -0,0 +1,17 @@
+<?php
+
+    // const. varaible---> value cannot be chnage in future
+
+    // syntax --> define(name,vale,csa-insensitive);
+
+    define("_num",500); //they are also known as global vaiable
+
+    echo _num;
+
+    // lets make in can insesitive
+
+    define("_test",125,true);
+
+    echo _Test
+
+?>
