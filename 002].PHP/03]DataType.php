aacd4fe5ac4f5a86c7f4984cee7791284d9aee35
Pawan Gupta <guptapawanro2017@gmail.com>
2021-10-29 08:01:54 +0530

    DataTypes

---
+++ 002].PHP/03]DataType.php
@@ -0,0 +1,16 @@
+<?php
+
+    $i = 100; //interger
+    $f = 10.2 ;// float
+    $s = "Hello world"; //string
+    $b = true; //Boolean
+    $x= array("hello",102,0.2,true); //array
+    $y = new MyClass() ; // object
+    $z = null; //null
+
+    echo i;
+    echo f;
+    echo s;
+    echo b;
+
+?>
