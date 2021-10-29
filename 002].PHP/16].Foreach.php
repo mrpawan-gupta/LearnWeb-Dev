93effb9db1e05a074babe83e5bc738882f397702
Pawan Gupta <guptapawanro2017@gmail.com>
2021-10-29 12:07:49 +0530

    For each

---
+++ 002].PHP/16].Foreach.php
@@ -0,0 +1,13 @@
+<?php
+
+    $age = [
+        "bill" => 25,
+        "steve" => 29,
+        "elon" => 12,
+    ];
+
+    foreach($age as $key => $value){
+        echo $key." = ".$value."<br/>";
+    }
+
+?>
