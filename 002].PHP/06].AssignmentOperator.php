1831a7be2b0f349c5f4b1b28c53bc0537745e80d
Pawan Gupta <guptapawanro2017@gmail.com>
2021-10-29 08:38:10 +0530

    Assignment Operator

--- 002].PHP/06].AssignmentOperator.php
+++ 002].PHP/06].AssignmentOperator.php
@@ -6,26 +6,32 @@
     // addition ;
     $a += $b;
     echo $a;
+    echo "<br>";
 
     //subtraction
-    $a -= $b;
+    $b -= $a;
     echo $b;
+    echo "<br>";
 
     // multiplication
     $a *= $b;
     echo $a;
+    echo "<br>";
 
     //division
     $a /= $b;
     echo $a;
+    echo "<br>";
 
     // mod 
     $a %= $b;
     echo $a;
+    echo "<br>";
 
     //exponentationn
     $a **= $b;
     echo $a;
+    echo "<br>";
 
 
 ?>
