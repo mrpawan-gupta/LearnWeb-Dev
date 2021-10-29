46e534a281fe13f6d0789f86ba3018bab1589985
Pawan Gupta <guptapawanro2017@gmail.com>
2021-10-29 08:28:34 +0530

    Arithmetic Operator

---
+++ 002].PHP/05].ArithmeticOperator.php
@@ -0,0 +1,31 @@
+<?php
+
+    $a = 10 ;
+    $b = 3 ;
+
+    // addition ;
+    $add = $a + $b;
+    echo $add;
+
+    //subtraction
+    $sub = $a-$b;
+    echo $sub;
+
+    // multiplication
+    $mul = $a*$b;
+    echo $mul;
+
+    //division
+    $div = $a/$b;
+    echo $div;
+
+    // mod 
+    $mod = $a%$b;
+    echo $mod;
+
+    //exponentationn
+    $expo = $a**$b;
+    echo $expo;
+
+
+?>
